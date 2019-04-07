e55b762dc19451cc5a4fa40e78c6c0c0a3974e4b
Kiyuno813 <47501344+Kiyuno813@users.noreply.github.com>
2019-04-07 19:41:37 -0300

    Update ESP32_Gmail_Sender.ino

--- Script Novo/ESP32_Gmail_Sender.ino
+++ Script Novo/ESP32_Gmail_Sender.ino
@@ -1,25 +1,20 @@
-/* Arduino core for ESP32 WiFi chip SMTP Gmail sender
-   by jubaid hossain
-   4.9.2018
-   you can copy and paste full code
-*/
 #include <WiFi.h>
 #include "Gsender_32.h"
 #include <WiFiClientSecure.h>
 
 #pragma region Globals
-const char* ssid = "ssid_name";                           // WIFI network name
-const char* password = "wifi_password";                       // WIFI network password
+const char* ssid = "REDE 3 2.4G";                           // WIFI network name
+const char* password = "ponto1244";                       // WIFI network password
 uint8_t connection_state = 0;                    // Connected to WIFI or not
 uint16_t reconnect_interval = 10000;             // If not connected wait time to try again
 #pragma endregion Globals
 
-String address[] = {"email1", "email2"};
+String address[] = {"brunohiroyukioda@hotmail.com", "herobrine813@gmail.com"};
 
 uint8_t WiFiConnect(const char* nSSID = nullptr, const char* nPassword = nullptr)
 {
   static uint16_t attempt = 0;
-  Serial.print("Connecting to ");
+  Serial.print("Conectando a rede ");
   if (nSSID) {
     WiFi.begin(nSSID, nPassword);
     Serial.println(nSSID);
@@ -37,14 +32,14 @@ uint8_t WiFiConnect(const char* nSSID = nullptr, const char* nPassword = nullptr
   ++attempt;
   Serial.println("");
   if (i == 51) {
-    Serial.print("Connection: TIMEOUT on attempt: ");
+    Serial.print("Conexão: TIMEOUT na tentativa: ");
     Serial.println(attempt);
     if (attempt % 2 == 0)
-      Serial.println("Check if access point available or SSID and Password\r\n");
+      Serial.println("Verifique se a rede está dispoível ou se o SSD e Senha estão corretos\r\n");
     return false;
   }
-  Serial.println("Connection: ESTABLISHED");
-  Serial.print("Got IP address: ");
+  Serial.println("Conexão: ESTABELECIDA");
+  Serial.print("Endereço de IP: ");
   Serial.println(WiFi.localIP());
   return true;
 }
@@ -62,25 +57,100 @@ void Awaits()
   }
 }
 
+//Ultrassônico:
+
+// Define o numero dos pinos do ultrassonico
+const int trigPin = 22;
+const int echoPin = 23;
+
+// Define as variáveis
+long duration;
+int distance;
+
+//--------------------------------------------//s
+
+
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   connection_state = WiFiConnect();
   if (!connection_state) // if not connected to WIFI
   {
     Awaits(); // constantly trying to connect
   }
-  Gsender *gsender = Gsender::Instance();    // Getting pointer to class instance
-  String subject = "ESP32 Test!";
-  if (gsender->Subject(subject)->Send(2, address, "ESP32 Test Email!"))
-  {
-    Serial.println("Message send.");
-  }
-  else
-  {
-    Serial.print("Error sending message: ");
-    Serial.println(gsender->getError());
-  }  
+
+  //Ultrassônico
+  pinMode(trigPin, OUTPUT); // Difine o trigPin como Output
+  pinMode(echoPin, INPUT); // Define o echoPin como Input
+  
+
 }
 
-void loop() {}
+void loop() {
+
+  //Ultrassônico:
+	// Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delay(2000);
+  
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  if (distance < 15){
+    
+    Serial.print("ARMÁRIO ABERTO ");
+    Gsender *gsender = Gsender::Instance();    // Getting pointer to class instance
+    String subject = "Seu armário foi aberto.";
+    if (gsender->Subject(subject)->Send(2, address, "A porta do seu armário foi aberta"))
+    {
+      Serial.println("Mensagem enviada.");
+    }
+    else
+    {
+      Serial.print("Erro ao enviar a mensagem: ");
+      Serial.println(gsender->getError());
+    }  
+  }
+	
+	while (distance < 15){
+  
+    Serial.println ("TO TRAVADO");
+
+    //NOVA LEITURA PARA DESTRAVAR
+
+    //Ultrassônico:
+    // Clears the trigPin
+    digitalWrite(trigPin, LOW);
+    delay(2000);
+  
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    // Calculating the distance
+    distance= duration*0.034/2;
+    // Prints the distance on the Serial Monitor
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    
+    
+	}
+	
+	
+	
+	
+	
+}
